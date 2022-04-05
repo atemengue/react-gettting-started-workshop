0ef0340a6fae10f1cf263cbd25bf85a9b896bbd4
atemengue <moafembe@gmail.com>
2022-04-05 14:36:05 +0100

    add home

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,25 @@
 /** @format */
 
-import React from 'react';
+import React, { useState } from 'react';
+import Field from '../components/Field';
+import Footer from '../components/Footer/Footer';
+import Header from '../components/header/Header';
+import Speakers from '../components/Speakers';
+import { data } from '../data';
 
-export default function Home() {
-  return <div>Accruil</div>;
-}
+const Home = () => {
+  const [term, setTerm] = useState('');
+
+  return (
+    <div className='ui'>
+      <Header />
+      <div className='ui container'>
+        <Field value={term} onChange={setTerm} />
+        <Speakers term={term} data={data} />
+      </div>
+      <div class='ui divider'></div>
+      <Footer />
+    </div>
+  );
+};
+export default Home;
